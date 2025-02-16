c90aa70d3b923de56452f13827e7afbc73bccff5
Hao Ting <boscoting0522@gmail.com>
2025-02-16 16:58:34 +0800

    refac: Use variables in .env

--- backend/config/db.go
+++ backend/config/db.go
@@ -1,8 +1,11 @@
 package config
 
 import (
-	"OneAI-mini-project/backend/models"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,13 +13,24 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("POSTGRES_SERVER"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
 	db, err := gorm.Open(
-		postgres.Open("postgres://orca:orca@localhost:5432/postgres"),
+		postgres.Open(dsn),
 		&gorm.Config{},
 	)
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&models.Story{})
 	DB = db
 }
